c8c42002d9b5c82cddcd172241e913ed2d2a4131
JishikaA <135000525+Daya2673@users.noreply.github.com>
2023-05-30 14:06:53 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:simplecalculator/calculator.dart';
+void main(){
+  runApp(MaterialApp(home:calc(),
+  debugShowCheckedModeBanner: false,));
+}
+
